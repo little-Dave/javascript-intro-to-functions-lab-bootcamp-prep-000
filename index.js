09bdf46bc246111690812817ab1239ad490ffa4c
Dave Boehmer <daboehmer@gmail.com>
2019-03-28 14:41:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    return ("I can't hear you!");
+    return "I can't hear you!";
   } elsif (string === string.toUpperCase()) {
-    return ("YES INDEED!");
+    return "YES INDEED!";
   } elsif (string === "I love your, Grandma.") {
-    return ("I love you, too.");
+    return "I love you, too.";
   }
 }
 
